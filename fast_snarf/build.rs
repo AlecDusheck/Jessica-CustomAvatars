dc008952f78dae7970809f914a3868ddfeb6c911
Alec Dusheck <a.dusheck@me.com>
2024-08-28 15:43:12 -0500

    update Rust C++ build scripts to properly handle slightly different PyTorch setups

--- fast_snarf/build.rs
+++ fast_snarf/build.rs
@@ -4,15 +4,17 @@ use std::path::PathBuf;
 fn main() {
     let cuda = PathBuf::from(env::var("CUDA_PATH").unwrap_or("/usr/local/cuda".to_string()));
     let cuda_include = cuda.join("include");
-    let libtorch = PathBuf::from("/usr/local/libtorch");
+    let libtorch = PathBuf::from(env::var("LIBTORCH").unwrap_or("/usr/local/libtorch".to_string()));
 
     println!("cargo:warning=CUDA_PATH: {:?}", cuda);
     println!("cargo:warning=libtorch path: {:?}", libtorch);
 
+    // TODO: remove, was having issues with my PyTorch install
+    println!("cargo:warning=libtorch include path: {:?}", libtorch.join("include"));
+    println!("cargo:warning=torch/all.h exists: {:?}", libtorch.join("include/torch/all.h").exists());
+
     println!("cargo:rustc-link-search=native={}", cuda.join("lib64").display());
     println!("cargo:rustc-link-search=native={}", libtorch.join("lib").display());
-
-    // println!("cargo:rustc-link-arg=-Wl,--copy-dt-needed-entries");
 
     // Link libraries
     println!("cargo:rustc-link-lib=stdc++");
@@ -24,49 +26,51 @@ fn main() {
     println!("cargo:rustc-link-lib=c10_cuda");
     println!("cargo:rustc-link-lib=gomp");
 
-    // Rest of your build script...
-    cc::Build::new()
+    let mut build = cc::Build::new();
+    build
         .cuda(true)
         .cpp(true)
         .include(&cuda_include)
         .include(&libtorch.join("include"))
-        .include("/usr/include/python3.10")
+        .include(&libtorch.join("include/torch"))
+        .include(&libtorch.join("include/torch/csrc/api/include"))
+        // TODO: Auto detect Python version and path
+        // .include("/usr/include/python3.10")
+        .include("/usr/include/python3.12")
+        .flag("-std=c++17")
+        .flag("--expt-relaxed-constexpr");
+
+    // Set CUDA architecture flags if TORCH_CUDA_ARCH_LIST is set
+    if let Ok(arch_list) = env::var("TORCH_CUDA_ARCH_LIST") {
+        for arch in arch_list.split(',') {
+            build.flag(&format!("-gencode=arch=compute_{0},code=sm_{0}", arch));
+        }
+    }
+
+    build
         .file("src/cuda/c_filter.cpp")
         .file("src/cuda/c_filter_kernel.cu")
-        .flag("-std=c++17")
-        .flag("--expt-relaxed-constexpr")
         .compile("c_filter");
 
-    cc::Build::new()
-        .cuda(true)
-        .cpp(true)
-        .include(&cuda_include)
-        .include(&libtorch.join("include"))
-        .include("/usr/include/python3.10")
+    build
         .file("src/cuda/c_fuse.cpp")
         .file("src/cuda/c_fuse_kernel.cu")
-        .flag("-std=c++17")
-        .flag("--expt-relaxed-constexpr")
         .compile("c_fuse");
 
-    cc::Build::new()
-        .cuda(true)
-        .cpp(true)
-        .include(&cuda_include)
-        .include(&libtorch.join("include"))
-        .include("/usr/include/python3.10")
+    build
         .file("src/cuda/c_precompute.cpp")
         .file("src/cuda/c_precompute_kernel.cu")
-        .flag("-std=c++17")
-        .flag("--expt-relaxed-constexpr")
         .compile("c_precompute");
 
     println!("cargo:rerun-if-changed=src/cuda/c_filter.cpp");
     println!("cargo:rerun-if-changed=src/cuda/c_filter_kernel.cu");
-
     println!("cargo:rerun-if-changed=src/cuda/c_fuse.cpp");
     println!("cargo:rerun-if-changed=src/cuda/c_fuse_kernel.cu");
-
     println!("cargo:rerun-if-changed=src/cuda/c_precompute.cpp");
     println!("cargo:rerun-if-changed=src/cuda/c_precompute_kernel.cu");
+
+    // Rerun if certain environment variables change
+    println!("cargo:rerun-if-env-changed=CUDA_PATH");
+    println!("cargo:rerun-if-env-changed=LIBTORCH");
+    println!("cargo:rerun-if-env-changed=TORCH_CUDA_ARCH_LIST");
 }
